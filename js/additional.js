06811762460d7a4b36d595d80a13823a1044b309
wooseok <wooseok@kudos.co.nz>
2022-11-09 10:31:57 +1300

    Add more images and sort them into tabs

---
+++ js/additional.js
@@ -0,0 +1,14 @@
+//Players
+var hostuwuming = document.getElementsByClassName("hostuwuming");
+var parallelken = document.getElementsByClassName("parallelken");
+var kyowo = document.getElementsByClassName("kyowo");
+var imhowotep = document.getElementsByClassName("imhowotep");
+//Game
+var minecraft = document.getElementsByClassName("minecraft");
+var borderlands = document.getElementsByClassName("borderlands");
+var deeprock = document.getElementsByClassName("deeprock");
+var nomansky = document.getElementsByClassName("nomansky");
+var forest = document.getElementsByClassName("forest");
+var valheim = document.getElementsByClassName("valheim");
+var overcooked  = document.getElementsByClassName("overcooked");
+
